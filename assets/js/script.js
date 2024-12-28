8db3f18846e3576185a4cc008562f23dcdaef36f
Punyah Baghla <punyahbaghla5@gmail.com>
2024-12-28 21:59:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,37 +47,11 @@ document.getElementById('water-quality-form').addEventListener('submit', functio
     let score = 0;
 
     const ph = parseFloat(document.getElementById('ph').value);
-    const doValue = parseFloat(document.getElementById('do').value);
-    const turbidity = parseFloat(document.getElementById('turbidity').value);
-    const ec = parseFloat(document.getElementById('ec').value);
-    const temperature = parseFloat(document.getElementById('temperature').value);
-    const tds = parseFloat(document.getElementById('tds').value);
 
-    // Check pH value
+  
     if (ph >= 6.5 && ph <= 8.5) score += 2;
     else if (ph >= 5.5 && ph < 6.5 || ph > 8.5 && ph <= 9.5) score += 1;
 
-    // Check Dissolved Oxygen value
-    if (doValue > 6) score += 2;
-    else if (doValue >= 4 && doValue <= 6) score += 1;
-
-    // Check Turbidity
-    if (turbidity <= 5) score += 2;
-    else if (turbidity > 5 && turbidity <= 20) score += 1;
-
-    // Check Electrical Conductivity
-    if (ec < 250) score += 2;
-    else if (ec >= 250 && ec <= 500) score += 1;
-
-    // Check Temperature
-    if (temperature >= 10 && temperature <= 25) score += 2;
-    else if (temperature > 25 && temperature <= 35) score += 1;
-
-    // Check TDS
-    if (tds < 500) score += 2;
-    else if (tds >= 500 && tds <= 1000) score += 1;
-
-    // Determine the result based on the score
     let result;
     let sliderValue;
     let color;
